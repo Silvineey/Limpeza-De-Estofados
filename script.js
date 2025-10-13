091ffa95d04b9d699764b5f7cb73f3a948b4b2c8
Silvineey <lucreciasilvio@hotmail.com>
2025-10-13 16:46:34 -0300

    falta pouco
    falta pouco

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-const slider=document.querySelectorAll("slider")
-const btnprev=document.getElementById("prev-button")
-const btnnext=document.getElementByIde("nex-button")
-let currentslide=0
-
-function ocultar(){
-    slider.forEach(item =>item.classList.remove("on"))
-}
-function mostrar(){
-    slider=[currentslide].classList.add("on")
+function galeria(){
+    const menu=document.getElementById("cama")
+    menu.classList.toggle("ativo")
 }
