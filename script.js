a0018b3552a1a4b4a4f04b3458b8a81591a5b99e
Silvineey <lucreciasilvio@hotmail.com>
2025-10-14 10:09:36 -0300

    cabo
    cabo

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-function galeria(){
-    const menu=document.getElementById("cama")
-    menu.classList.toggle("ativo")
+function sofas(){
+    esconder()
+    const galeria=document.getElementById("sofa")
+    galeria.classList.toggle("ativo")
+}
+function camas(){
+    esconder()
+    const galeria=document.getElementById("cama")
+    galeria.classList.toggle("ativo")
+}
+function cadeiras(){
+    esconder()
+    const galeria=document.getElementById("cadeira")
+    galeria.classList.toggle("ativo")
+}
+
+function esconder(){
+    const search=document.querySelectorAll(".categoria .oculto")
+    search.forEach(c=>{
+        c.classList.remove("ativo")
+    })
 }
