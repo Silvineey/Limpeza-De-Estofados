d59e67cafa79073c5ecf7bb09dbe7a958acfc4f1
Silvineey <lucreciasilvio@hotmail.com>
2025-10-28 14:37:00 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-function sofas(){
-    esconder()
-    const galeria=document.getElementById("sofa")
-    galeria.classList.toggle("ativo")
-}
-function camas(){
-    esconder()
-    const galeria=document.getElementById("cama")
-    galeria.classList.toggle("ativo")
-}
-function cadeiras(){
-    esconder()
-    const galeria=document.getElementById("cadeira")
-    galeria.classList.toggle("ativo")
-}
-
-function esconder(){
-    const search=document.querySelectorAll(".categoria .oculto")
-    search.forEach(c=>{
-        c.classList.remove("ativo")
-    })
+function abrir(){
+    const img=document.querySelector(".barramobile")
+    if(img.style.display==="flex"){
+        img.style.display="none"
+    }
+    else{
+        img.style.display="flex"
+    }
 }
