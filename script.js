e61c599db5c855ccdc14fd7c4942812ee732467e
Silvineey <lucreciasilvio@hotmail.com>
2025-12-09 08:40:03 -0300

    ajustes
    ajustes

--- script.js
+++
@@ -1,9 +0,0 @@
-function abrir(){
-    const img=document.querySelector(".barramobile")
-    if(img.style.display==="flex"){
-        img.style.display="none"
-    }
-    else{
-        img.style.display="flex"
-    }
-}
